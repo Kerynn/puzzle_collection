80a54a4e93d8d7a30dc7913073124cf242eceed4
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-29 15:38:30 -0800

    Add and pass first test for display puzzle by id

--- spec/features/puzzles/show_spec.rb
+++ spec/features/puzzles/show_spec.rb
@@ -2,28 +2,31 @@ require 'rails_helper'
 
 RSpec.describe 'the puzzles show page' do 
 
-# As a visitor
+# As a visitor INDEX
 # When I visit '/puzzles'
 # Then I see each puzzle name including pieces_count and put_togehter
 
-# As a visitor
-# When I visit '/puzzles/1' (or id of the puzzle)
-# Then I see the puzzle with that id including the pieces_count and put_together
-
-# As a visitor
+# As a visitor INDEX
 # When I visit '/collectors/:collector_id/puzzles'
 # Then I see each puzzle that is associated with that Collector with each puzzle's attributes
 # (data from each column that is on the child table)
 
-  it 'displays the puzzle' do 
-    puzzle = Puzzle.create!(name: "Star Wars", pieces_count: 1000, put_together: true)
-    visit "/puzzles/#{puzzle.id}"
+# As a visitor SHOW
+# When I visit '/collectors/:id'
+# Then I see the Collector name with that id including the skills rating and if under 30
 
+  it 'displays the puzzle by id' do 
+    collector = Collector.create!(name: "Charlie Moore", skills_rating: 5, under_30_yrs: false)
+    starwars = collector.puzzles.create!(name: "Star Wars", pieces_count: 1000, put_together: true)
+    rainbowcookies = collector.puzzles.create!(name: "Rainbow Cookies", pieces_count: 300, put_together: true)
+   
+    visit "/puzzles/#{starwars.id}"
+
+    expect(page).to have_content(starwars.name)
+    expect(page).to have_content(starwars.pieces_count)
+    expect(page).to have_content(starwars.put_together)
+    expect(page).to_not have_content(rainbowcookies.name)
   end 
 
-  xit 'displays the name of collector of the puzzle' do 
-
-
-  end 
 
 end 
