c99fa1ef7c6b0db5e4c3fccdef83d56fdfe5e700
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-29 19:21:55 -0800

    Delete user stories no longer need

--- spec/features/puzzles/show_spec.rb
+++ spec/features/puzzles/show_spec.rb
@@ -2,19 +2,6 @@ require 'rails_helper'
 
 RSpec.describe 'the puzzles show page' do 
 
-# As a visitor INDEX
-# When I visit '/puzzles'
-# Then I see each puzzle name including pieces_count and put_togehter
-
-# As a visitor INDEX
-# When I visit '/collectors/:collector_id/puzzles'
-# Then I see each puzzle that is associated with that Collector with each puzzle's attributes
-# (data from each column that is on the child table)
-
-# As a visitor SHOW Need to create a collectors folder in features to then test
-# When I visit '/collectors/:id'
-# Then I see the Collector name with that id including the skills rating and if under 30
-
   it 'displays the puzzle by id' do 
     collector = Collector.create!(name: "Charlie Moore", skills_rating: 5, under_30_yrs: false)
     starwars = collector.puzzles.create!(name: "Star Wars", pieces_count: 1000, put_together: true)
