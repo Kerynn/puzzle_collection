5ee1d75a5cd546ffea3b58ebc13d53a59cccfd4a
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-30 13:43:24 -0800

    Add route for collectors show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/collectors', to: 'collectors#index'
+  get '/collectors/:id', to: 'collectors#show'
   get '/puzzles', to: 'puzzles#index'
   get '/puzzles/:id', to: 'puzzles#show'
 end
