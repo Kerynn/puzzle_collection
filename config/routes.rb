caf9986daee12b5e1fd3cfb6381d46d30f5998c8
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-29 16:44:07 -0800

    Delete incorrectly named controller file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get '/collectors', to: 'collectors#index'
   get '/puzzles', to: 'puzzles#index'
   get '/puzzles/:id', to: 'puzzles#show'
 end
