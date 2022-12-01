9139b9e3f959757326ee4e45d550852e817f8714
Kerynn Davis <kerynn.davis@gmail.com>
2022-11-30 18:12:02 -0800

    Add and pass test for collector index sorted by recently created

--- spec/features/collectors/index_spec.rb
+++ spec/features/collectors/index_spec.rb
@@ -11,6 +11,30 @@ RSpec.describe "collectors index page" do
     expect(page).to have_content(collector_2.name)
   end 
 
+  # As a visitor
+  # When I visit the parent index,
+  # I see that records are ordered by most recently created first
+  # And next to each of the records I see when it was created
 
+  xit 'collectors are ordered by most recently created first' do 
+    collector_1 = Collector.create!(name: "Charlie Moore", skills_rating: 5, under_30_yrs: false)
+    collector_2 = Collector.create!(name: "Leslie Grant", skills_rating: 8, under_30_yrs: true)
+    collector_3 = Collector.create!(name: "Nicole Wofford", skills_rating: 10, under_30_yrs: true)
+   
+    visit '/collectors'
 
+    # expect(page).should have_selector("ul#dates")
+  end 
+
+  it 'shows when the collector was created next to each collector' do 
+    collector_1 = Collector.create!(name: "Charlie Moore", skills_rating: 5, under_30_yrs: false)
+    collector_2 = Collector.create!(name: "Leslie Grant", skills_rating: 8, under_30_yrs: true)
+    collector_3 = Collector.create!(name: "Nicole Wofford", skills_rating: 10, under_30_yrs: true)
+   
+    visit '/collectors'
+
+    expect(page).to have_content("Created: #{collector_1.created_at}")
+    expect(page).to have_content("Created: #{collector_2.created_at}")
+    expect(page).to have_content("Created: #{collector_3.created_at}")
+  end 
 end 
