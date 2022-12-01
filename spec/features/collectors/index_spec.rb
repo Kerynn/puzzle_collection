b8c1e7ef80a096324cc6297e57295c2edcf08f80
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-01 12:46:19 -0800

    Add test and pass collector index page to order by most recently created

--- spec/features/collectors/index_spec.rb
+++ spec/features/collectors/index_spec.rb
@@ -16,14 +16,16 @@ RSpec.describe "collectors index page" do
   # I see that records are ordered by most recently created first
   # And next to each of the records I see when it was created
 
-  xit 'collectors are ordered by most recently created first' do 
+  it 'collectors are ordered by most recently created first' do 
     collector_1 = Collector.create!(name: "Charlie Moore", skills_rating: 5, under_30_yrs: false)
+    sleep(1) 
     collector_2 = Collector.create!(name: "Leslie Grant", skills_rating: 8, under_30_yrs: true)
     collector_3 = Collector.create!(name: "Nicole Wofford", skills_rating: 10, under_30_yrs: true)
    
     visit '/collectors'
 
-    # expect(page).should have_selector("ul#dates")
+    expect(collector_3.name).to appear_before(collector_2.name)
+    expect(collector_2.name).to appear_before(collector_1.name)
   end 
 
   it 'shows when the collector was created next to each collector' do 
