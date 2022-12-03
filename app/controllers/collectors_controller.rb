3d6fcaf57a9f2f3e6b7ca7214434725df2d901d7
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-02 16:12:01 -0800

    Adds collector new and create features

--- app/controllers/collectors_controller.rb
+++ app/controllers/collectors_controller.rb
@@ -6,4 +6,15 @@ class CollectorsController < ApplicationController
   def show 
     @collector = ::Collector.find(params[:id])
   end 
+
+  def new 
+  end 
+
+  def create 
+    collector = ::Collector.create(
+      name: params[:name],
+      skills_rating: params["Skills Rating"].to_i,
+      under_30_yrs: params["Under 30 Years Old"])
+    redirect_to "/collectors"
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/collectors', to: 'collectors#index'
+  get '/collectors/new', to: 'collectors#new'
   get '/collectors/:id', to: 'collectors#show'
   get '/puzzles', to: 'puzzles#index'
   get '/puzzles/:id', to: 'puzzles#show'
   get '/collectors/:collector_id/puzzles', to: 'collector_puzzles#index'
+  post '/collectors', to: 'collectors#create'
 end
 
