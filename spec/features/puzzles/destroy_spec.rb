21a111da28e360221485d7ce460d74df78b7bd12
Kerynn Davis <kerynn.davis@gmail.com>
2022-12-04 13:58:34 -0800

    test: Add test to delete a puzzle from the puzzle index page

--- spec/features/puzzles/destroy_spec.rb
+++ spec/features/puzzles/destroy_spec.rb
@@ -25,4 +25,34 @@ RSpec.describe 'destroy a puzzle' do
       expect(page).to_not have_content(buffalo.name)
     end 
   end 
+
+  describe 'when I visit the puzzle index page' do 
+    it 'shows a link next to every puzzle to delete the puzzle' do 
+      caleb = Collector.create!(name: "Caleb Faleb", skills_rating: 7, under_30_yrs: false)
+      buffalo = caleb.puzzles.create!(name: "Buffalo Roams", pieces_count: 600, put_together: true)
+      snow = caleb.puzzles.create!(name: "Snowy Mountains", pieces_count: 800, put_together: true)
+
+      visit "/puzzles"
+      
+      expect(page).to have_link("Delete #{buffalo.name}")
+      expect(page).to have_link("Delete #{snow.name}")
+    end
+
+    it 'deletes the puzzle and no longer displayed on the puzzle index page' do 
+      caleb = Collector.create!(name: "Caleb Faleb", skills_rating: 7, under_30_yrs: false)
+      buffalo = caleb.puzzles.create!(name: "Buffalo Roams", pieces_count: 600, put_together: true)
+      snow = caleb.puzzles.create!(name: "Snowy Mountains", pieces_count: 800, put_together: true)
+      
+      visit "/puzzles"
+      
+      expect(page).to have_content(buffalo.name)
+      expect(page).to have_content(snow.name)
+
+      click_link "Delete #{buffalo.name}"
+    
+      expect(current_path).to eq("/puzzles")
+      expect(page).to_not have_content(buffalo.name)
+      expect(page).to have_content(snow.name)
+    end 
+  end 
 end 
